15cd72aed97c40a1bb352404baab145cc71875d7
Julian Rosse <julian@helixbass.net>
2026-06-21 13:59:44 -0400

    working on wav loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,16 @@
-import WavPlayer from './WavPlayer'
-import {flowMax, SimplePropsAdder, addStateHandlers} from 'ad-hok'
+import { WavLoader } from './WavLoader'
+import {flowMax} from 'ad-hok'
 import {FC} from 'react'
-import {typedAs} from './utils/typedAs'
-import {addEffectOnMount, branchIfNullish} from 'ad-hok-utils'
 
 import './index.css'
-
-const EXAMPLE_WAV_FILE_URL = "/M1F1-float32WE-AFsp.wav"
-
-const loadWavFile = async ({onWavFileLoaded}: {
-  onWavFileLoaded: (wavFileContents: Blob) => void,
-}) => {
-  try {
-    const response = await fetch(EXAMPLE_WAV_FILE_URL)
-
-    if (!response.ok) {
-      throw new Error('Wav fetch failed')
-    }
-
-    const blob = await response.blob();
-
-    onWavFileLoaded(blob)
-  } catch (error) {
-    console.error("Loading wav file failed: ", error)
-  }
-}
-
-const addWavFileContents: SimplePropsAdder<{
-  wavFileContents: Blob,
-}> = flowMax(
-  addStateHandlers(
-    {
-      wavFileContents: typedAs<Blob | undefined>(undefined),
-    },
-    {
-      onWavFileLoaded: () => (wavFileContents: Blob) => ({
-        wavFileContents,
-      }),
-    }
-  ),
-  addEffectOnMount(
-    ({onWavFileLoaded}) => () => {
-      loadWavFile({onWavFileLoaded})
-    },
-  ),
-  branchIfNullish(
-    'wavFileContents',
-    {
-      returns: () => <div>loading</div>,
-    },
-  ),
-)
 
 interface Props {
 }
 
 const App: FC<Props> = flowMax(
-  addWavFileContents,
-  ({wavFileContents}) => (
+  () => (
     <>
-      <WavPlayer wavFileContents={wavFileContents} />
+      <WavLoader uuid="A52691A1-64AA-40C5-AEA8-9FD8C67230C4" />
     </>
   )
 )
