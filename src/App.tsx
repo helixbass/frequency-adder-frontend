b81871f794d3783d8297bccde0d1bebf491cce18
Julian Rosse <julian@helixbass.net>
2026-06-21 14:07:11 -0400

    setting up apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { WavLoader } from './WavLoader'
 import {flowMax} from 'ad-hok'
 import {FC} from 'react'
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client/react'
 
 import './index.css'
+
+const apolloClient = new ApolloClient({
+  link: new HttpLink({uri: "http://localhost:8080/graphql"}),
+  cache: new InMemoryCache(),
+})
 
 interface Props {
 }
@@ -10,7 +17,9 @@ interface Props {
 const App: FC<Props> = flowMax(
   () => (
     <>
-      <WavLoader uuid="A52691A1-64AA-40C5-AEA8-9FD8C67230C4" />
+      <ApolloProvider client={apolloClient}>
+        <WavLoader uuid="A52691A1-64AA-40C5-AEA8-9FD8C67230C4" />
+      </ApolloProvider>
     </>
   )
 )
